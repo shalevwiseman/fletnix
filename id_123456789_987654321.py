31c7757df17e0641667abc012e6698f1d383b1b6
shalevwiseman <shalevwisema@campus.technion.ac.il>
2023-05-27 18:54:53 +0300

    work with condition and copy

--- id_123456789_987654321.py
+++ id_123456789_987654321.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
+import copy
 # TODO: remove this import
 # from scipy.stats import beta
 
@@ -15,25 +16,22 @@ class Planner:
         self.phase_len = phase_len
         self.num_arms = num_arms
         self.num_users = num_users
-        self.arms_thresh = arms_thresh
-        self.phase_counter = 0
+        self.arms_thresh = copy.deepcopy(arms_thresh)
+        self.OG_arms_thresh = copy.deepcopy(arms_thresh)
         self.users_distribution = users_distribution
         self.users_alpha = np.full((num_users, num_arms), 0.1)
         self.users_beta = np.full((num_users, num_arms), 0.1)
         self.users_max_score = np.zeros((num_users, num_arms))
         self.users_counter = np.zeros((num_users, num_arms))
-        self.users_satisfaction = np.zeros((num_users, num_arms))
-        self.users_mean = np.zeros((num_users, num_arms))
         self.arm_use_per_phase = np.zeros(num_arms)
         self.num_round_per_phase = 0
         self.current_user = None
         self.current_arm = None
-        self.current_round = 0
+        self.current_round = -1
         self.keep_arms = True
         self.keeping_length = self.num_rounds
-        self.round_to_thrash = arms_thresh
+        self.round_to_thrash = copy.deepcopy(arms_thresh)
         self.user_not_to_choose = {}
-
         self.deactivated_arms = set()
         for i in range(num_users):
             self.user_not_to_choose[i] = set()
@@ -51,24 +49,20 @@ class Planner:
         # update the current user
         self.current_user = user_context
 
-        # update the current round
         # checks for every phase round.
-        if self.current_round > 0 and self.current_round % self.phase_len == 0:
-            self.phase_counter += 1
+        if (self.current_round + 1) % self.phase_len == 0 and self.current_round > 0:
+            # reset the round counter
             self.num_round_per_phase = 0
+
 
             # for every arm check if it is deactivated
             for i in range(self.num_arms):
                 if self.arm_use_per_phase[i] < self.arms_thresh[i]:
                     self.deactivated_arms.add(i)
             self.arm_use_per_phase = np.zeros(self.num_arms)
-            self.round_to_thrash = self.arms_thresh
+            self.round_to_thrash = self.OG_arms_thresh.copy()
         if (len(self.deactivated_arms) == self.num_arms - 1):
             self.keep_arms = False
-
-        self.current_round += 1
-        # update the current phase
-        self.num_round_per_phase += 1
 
 
         if (self.keep_arms == True) and (self.current_round < self.keeping_length):
@@ -77,19 +71,27 @@ class Planner:
             for i in range(self.num_arms):
                 if self.round_to_thrash[i] > 0:
                     total_rounds_to_thrash += self.round_to_thrash[i]
-            if (self.num_round_per_phase > total_rounds_to_thrash):
+            if (self.phase_len - self.num_round_per_phase) > total_rounds_to_thrash:
                 self.max_sample()
                 self.arm_use_per_phase[self.current_arm] += 1
                 self.round_to_thrash[self.current_arm] -= 1
+                self.users_counter[self.current_user][self.current_arm] += 1
+                self.current_round += 1
+                # update the current phase
+                self.num_round_per_phase += 1
                 return self.current_arm
             else:
                 # TODO: think about somthing better
+
                 for i in range(self.num_arms):
                     if (i not in self.deactivated_arms) and (self.arm_use_per_phase[i] < self.arms_thresh[i]):
                         self.current_arm = i
                         self.users_counter[self.current_user][self.current_arm] += 1
                         self.arm_use_per_phase[self.current_arm] += 1
                         self.round_to_thrash[self.current_arm] -= 1
+                        self.current_round += 1
+                        # update the current phase
+                        self.num_round_per_phase += 1
                         return self.current_arm
 
         # TODO: think about the expectation of keep arm or not, if we want to keep, self.keeping_length =
@@ -97,16 +99,11 @@ class Planner:
         self.max_sample()
         self.arm_use_per_phase[self.current_arm] += 1
         self.round_to_thrash[self.current_arm] -= 1
+        self.users_counter[self.current_user][self.current_arm] += 1
+        self.current_round += 1
+        # update the current phase
+        self.num_round_per_phase += 1
         return self.current_arm
-
-
-
-
-
-
-
-
-
 
 
     def notify_outcome(self, reward):
